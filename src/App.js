ff396c1a66e3fe242660f061ca3f5557d5c7a0d3
Rodrigofray <rodrigofray1@gmail.com>
2022-05-23 20:30:14 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import  Button  from '@mui/material';
+function App() {
+  return (
+    <><Button variant="outlined">Outlined</Button>
+    <Button variant="outlined">Outlined</Button></>
+  );
+}
+
+export default App;
